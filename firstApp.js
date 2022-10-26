940c67095ed983f0f64266215d8f1d0113ed792b
Emmanuel <emmayodayo@gmail.com>
2022-10-26 15:12:26 +0100

    node is all about events

--- firstApp.js
+++ firstApp.js
@@ -5,6 +5,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
 	console.log(req)
+	process.exit()
 });
 
 server.listen(9000);
@@ -14,3 +15,15 @@ server.listen(9000);
 // A request listener is a function that executes for every incoming request to the server
 
 // Create server returns a server
+
+// Node program lifecycle
+// Node file is executed
+// Start script
+// The event loop is a loop process that keeps on runninhg as long as there is an event listener running
+
+// Node code is managed by this...
+// Node executes single threaded javascript
+
+// Behind the scenes node still leverages on the operating system for multithreading
+
+// If you were to unregister, it would end
