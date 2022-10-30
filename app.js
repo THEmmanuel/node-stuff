607fa38fe2b6649c6ef56b7db2a9c68a6e541c2b
Emmanuel <emmayodayo@gmail.com>
2022-10-30 04:31:30 +0100

    add routes

--- app.js
+++ app.js
@@ -3,19 +3,21 @@ const express = require('express')
 const app = express();
 // Express exprts a function by default
 
-app.use((req, res, next) => {
-	console.log('in the middleware')
-	next()
-	// Allows request in another middleware to run
+app.use('/', (req, res, next) => {
+	console.log('this always runs')
 })
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
 	console.log('in another middleware')
-	res.send('<h1>Hello from express</h1>'); 
+	res.send('<h1>Add product</h1>'); 
+})
+
+app.use('/', (req, res, next) => {
+	console.log('in another middleware')
+	res.send('<h1>Hello from express</h1>');
 })
 // use allows to add a new middleware function
 // Accepts an array of request handlers
 // Middleware: Add functions hooked into a funnel through which the request goes.
-
 
 app.listen(9000);
