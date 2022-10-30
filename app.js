ff2c7eda7dedda82746f26ce461346c7a53423ba
Emmanuel <emmayodayo@gmail.com>
2022-10-30 04:06:39 +0100

    middlewares

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const http = require('http');
 const express = require('express')
 
-const app = express()
+const app = express();
+// Express exprts a function by default
+
+app.use((req, res, next) => {
+	console.log('in the middleware')
+	next()
+	// Allows request in another middleware to run
+})
+
+app.use((req, res, next) => {
+	console.log('in another middleware')
+})
+// use allows to add a new middleware function
+// Accepts an array of request handlers
 
 const server = http.createServer(app);
 
