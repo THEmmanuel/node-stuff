8818aeb5af679f8f1d242118e110ad097134932f
Emmanuel <emmayodayo@gmail.com>
2022-11-01 02:29:31 +0100

    serving html pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router()
+const path = require('path');
 
 router.get('/add-product', (req, res, next) => {
-	console.log('in another middleware')
-	res.send('<form action = "/admin/add-product" method = "POST"><input type = "text" name = "title"/><button type = "submit"> Add product<button/></form>'); 
+	res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
 })
 
 router.post('/add-product', (req, res, next) => {
